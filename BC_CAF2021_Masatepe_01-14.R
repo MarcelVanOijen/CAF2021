a3ded80be6973affa9dfd078b41c4fa08435ec62
Marcel Van Oijen <marcelvanoijen@Marcels-iMac.local>
2021-05-20 16:14:19 +0100

    Data files Masatepe (Nicaragua), first version with only annual coffee yields

---
+++ BC_CAF2021_Masatepe_01-14.R
@@ -0,0 +1,16 @@
+## BC_CAF2021_Masatepe_01-14.R
+## MvO, 2021-05-20
+
+## 1. INITIALISE MCMC ##
+   # nChain <- as.integer(36000)
+   nChain <- as.integer(36)
+   source('BC/BC_CAF2021_MCMC_init_Masatepe_01-14.R')
+
+## 2. RUNNING THE MCMC ##
+   source('BC/BC_CAF2021_MCMC.R')
+
+## 3. PRINTING AND PLOTTING ##
+   source('BC/BC_export_parModes.R')
+   source('BC/BC_plot_parameters_traceplots.R')
+   source('BC/BC_plot_parameters_priorbeta_histograms.R')
+   source('BC/BC_plot_outputs_data.R')
